12b785d785bc9056d9b9fd7a38b192f45f774661
James Billingham <james@billingham.net>
2015-04-19 12:28:30 -0400

    data hookups

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+from os import listdir
+from os.path import isdir, join
 import json
 app = Flask(__name__)
 
@@ -8,24 +10,47 @@ def show_root():
 
 @app.route("/videos/")
 def show_videos():
-	return render_template('videos.html')
+	path = './data/videos/'
+	dirs = [ f for f in listdir(path) if isdir(join(path, f)) ]
+	videos = []
+	for f in dirs:
+		x = json.load(open(join(path, f, 'data.json'), 'r'))
+		x['id'] = f
+		videos.append(x)
+	return render_template('videos.html', videos=videos)
 
 @app.route("/videos/<int:video_id>/")
 def show_video(video_id):
-	return render_template('video.html')
+	video = json.load(open('./data/videos/' + str(video_id) + '/data.json', 'r'))
+	video['id'] = video_id
+	return render_template('video.html', video=video)
 
 @app.route("/videos/<int:video_id>/test/")
 def show_test(video_id):
-	return render_template('test.html')
+	video = json.load(open('./data/videos/' + str(video_id) + '/data.json', 'r'))
+	video['id'] = video_id
+	return render_template('test.html', video=video)
 
 @app.route("/videos/<int:video_id>/results/")
 def show_results(video_id):
-	return render_template('results.html')
+	video = json.load(open('./data/videos/' + str(video_id) + '/data.json', 'r'))
+	video['id'] = video_id
+	path = './data/videos/' + str(video_id) + '/results/'
+	dirs = [ f for f in listdir(path) if isdir(join(path, f)) ]
+	results = []
+	for f in dirs:
+		x = json.load(open(join(path, f, 'data.json'), 'r'))
+		x['id'] = f
+		results.append(x)
+	return render_template('results.html', results=results, video=video)
 
 @app.route("/videos/<int:video_id>/results/<int:result_id>/")
 def show_result(video_id, result_id):
-	file = open('./data/videos/' + str(video_id) + '/results/' + str(result_id) + '/data.json', 'r')
-	return render_template('result.html', result=json.load(file))
+	video = json.load(open('./data/videos/' + str(video_id) + '/data.json', 'r'))
+	video['id'] = video_id
+	path = './data/videos/' + str(video_id) + '/results/' + str(result_id) + '/data.json'
+	result = json.load(open(path, 'r'))
+	return render_template('result.html', result=result, video=video)
 
 if __name__ == "__main__":
 	app.run(debug=True)
